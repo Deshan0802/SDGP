6d9fc4c97fc7fe3c06e7a260766dcd8846058492
Deshan0802 <d.deshan0802@gmail.com>
2024-03-03 13:59:25 +0530

    Create connection to user_accounts database

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route("/")
-def method_one():
-    return "<p>Root</p>"
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'user_accounts'
 
-@app.route("/text-to-asl")
-def method_two():
-    return "<p>text-to-asl</p>"
+mysql = MySQL(app)
 
-@app.route("/audio-to-asl")
-@app.route("/video-to-asl")
-def method_three():
-    return "<p>audio-to-asl, video-to-asl</p>"
+@app.route('/')
+def index():
+    if mysql.connection:
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM users")
+        users = cursor.fetchall()
+        cursor.close()
+        return jsonify(users)
+    else:
+        return '<p>Failed to connect to the database.</p>'
 
 if __name__ == "__main__":
     app.run(debug=True)
