154ce4fa2f2bf3a390b426cef073cbd070b18995
Deshan0802 <d.deshan0802@gmail.com>
2024-03-04 20:49:00 +0530

    Add backend logic to create new user accounts and add to user_accounts database

--- backend/main.py
+++ backend/main.py
@@ -17,15 +17,28 @@ def register_user():
     username = data.get("username")
     password = data.get("password")
     password_confirmed = data.get("password_confirmed")
-    return jsonify({"message": "Registration successful"}), 200
-    # if mysql.connection:
-    #     cursor = mysql.connection.cursor()
-    #     cursor.execute("SELECT * FROM users")
-    #     users = cursor.fetchall()
-    #     cursor.close()
-    #     return jsonify(users)
-    # else:
-    #     return ""
+    if mysql.connection:
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        result = cursor.fetchone()
+        if result is None:
+            if password == password_confirmed:
+                cursor.execute(
+                    "INSERT INTO users (username, password) VALUES (%s, %s)",
+                    (
+                        username,
+                        password,
+                    ),
+                )
+                mysql.connection.commit()
+                cursor.close()
+                return jsonify({"message": "Registration successful"}), 200
+        else:
+            cursor.close()
+            return jsonify({"error": "Username is already taken"}), 400
+
+    else:
+        return jsonify({"error": "Cannot establish a connection to the database"}), 400
 
 
 @app.route("/login", methods=["POST"])
@@ -37,7 +50,7 @@ def login():
     password = request.json.get("password", "")
 
     if username == "example_username" and password == "example_password":
-       
+
         token = "example_token"
         return jsonify({"message": "Login successful", "token": token}), 200
     else:
