dba6e4946521f819a9b4e8f396f3be6d88dffbc6
Deshan0802 <d.deshan0802@gmail.com>
2024-03-04 17:31:53 +0530

    Create route in backend for registering new users

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,32 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'user_accounts'
-
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "user_accounts"
 mysql = MySQL(app)
 
-@app.route('/')
-def index():
-    if mysql.connection:
-        cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM users")
-        users = cursor.fetchall()
-        cursor.close()
-        return jsonify(users)
-    else:
-        return '<p>Failed to connect to the database.</p>'
+
+@app.route("/register", methods=["POST"])
+def register_user():
+    if not request.is_json:
+        return jsonify({"error": "Request must be JSON"}), 400
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    password_confirmed = data.get("password_confirmed")
+    return jsonify({"message": "Registration successful"}), 200
+    # if mysql.connection:
+    #     cursor = mysql.connection.cursor()
+    #     cursor.execute("SELECT * FROM users")
+    #     users = cursor.fetchall()
+    #     cursor.close()
+    #     return jsonify(users)
+    # else:
+    #     return ""
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
