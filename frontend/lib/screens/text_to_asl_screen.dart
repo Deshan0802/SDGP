2ff7f9c2d8e39500a1e5a8d15048ba03c8188df3
Hirushan Pathirage <hirushanp2004@gmail.com>
2024-03-13 16:24:37 +0530

    Update 'reusable.dart' by removing all unwanted code snippets and, delete unwanted 'newTextToASL.dart' file.

--- frontend/lib/screens/text_to_asl_screen.dart
+++ frontend/lib/screens/text_to_asl_screen.dart
@@ -46,24 +46,25 @@ class TextToASLState extends State<TextToASL> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 220, 220, 220),
+      resizeToAvoidBottomInset: true,
       appBar: const CustomAppBar(
         headerText: 'Text to ASL',
         bottomSheetContent: ('Converts Input Text Into ASL'),
       ),
       body: SafeArea(
         child: SingleChildScrollView(
+          reverse: true,
           child: Container(
             margin: const EdgeInsets.all(20.0),
             child: Column(
               children: <Widget>[
-                const SizedBox(height: 10),
+                const SizedBox(height: 30),
 
                 //ASL output section
                 Column(
                   children: [
                     ClipRRect(
-                      borderRadius: BorderRadius.circular(17),
+                      borderRadius: BorderRadius.circular(15),
                       child: SizedBox(
                         height: 300,
                         width: 290,
@@ -84,75 +85,135 @@ class TextToASLState extends State<TextToASL> {
                       ),
                     ),
                     const SizedBox(height: 10),
-                    FloatingActionButton(
-                      onPressed: () {
-                        setState(() {
-                          if (_videoController.value.isPlaying) {
-                            _videoController.pause();
-                          } else {
-                            _videoController.play();
-                          }
-                        });
-                      },
-                      backgroundColor: Colors.black,
-                      foregroundColor: Colors.white,
-                      mini: true,
-                      child: Icon(
-                        _videoController.value.isPlaying
-                            ? Icons.pause
-                            : Icons.play_arrow,
+                    SizedBox(
+                      width: 100,
+                      height: 40,
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_videoController.value.isPlaying) {
+                              _videoController.pause();
+                            } else {
+                              _videoController.play();
+                            }
+                          });
+                        },
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                _videoController.value.isPlaying
+                                    ? Icons.pause
+                                    : Icons.play_arrow,
+                              ),
+                              const SizedBox(width: 5),
+                              Text(
+                                _videoController.value.isPlaying
+                                    ? 'Pause'
+                                    : 'Play',
+                              ),
+                              const SizedBox(width: 6),
+                            ],
+                          ),
+                        ),
                       ),
                     ),
                   ],
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 30),
 
                 //English text input section
                 Column(
                   children: [
-                    CustomWhiteBox(
-                      whiteBox: <Widget>[
-                        Container(
-                          height: 35,
-                          width: 90,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.black,
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 7,
+                            offset: Offset(0, 2),
                           ),
-                          child: const Center(
-                            child: Text(
-                              'English',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              const SizedBox(width: 10),
+                              Container(
+                                height: 40,
+                                width: 90,
+                                decoration: BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: const Center(
+                                    child: Text(
+                                  'English',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                )),
+                              )
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextField(
+                              controller: _textEditingController,
+                              maxLines: 2,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: 310,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: ElevatedButton.icon(
+                              onPressed: () {
+                                _textEditingController.clear();
+                              },
+                              icon: const Icon(Icons.clear_all),
+                              label: const Text("Clear All"),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    const Color.fromARGB(255, 137, 137, 137),
+                                foregroundColor: Colors.white,
                               ),
                             ),
                           ),
-                        ),
-                        TextField(
-                          controller: _textEditingController,
-                          maxLines: 3,
-                        ),
-                      ],
-                      verticalMargin: 10.0,
-                      horizontalMargin: 0.0,
-                      verticalPadding: 15.0,
-                      horizontalPadding: 15.0,
-                    ),
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        _sendTextToBackend(_textEditingController.text);
-                      },
-                      icon: const Icon(Icons.refresh),
-                      label: const Text("Convert"),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.black, // Background color
-                        foregroundColor: Colors.white, // Text color
+                          const SizedBox(width: 30),
+                          Expanded(
+                            flex: 1,
+                            child: ElevatedButton.icon(
+                              onPressed: () {
+                                _sendTextToBackend(_textEditingController.text);
+                              },
+                              icon: const Icon(Icons.refresh),
+                              label: const Text("Convert"),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.black,
+                                foregroundColor: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
-                const SizedBox(height: 20),
               ],
             ),
           ),
