4ffc19718bb3893b85d482039d8adf3b1b08d169
Deshan0802 <d.deshan0802@gmail.com>
2024-03-14 21:59:06 +0530

    Remove blueprints with issues and made changes to use the hosted Database for user authentication.

--- signwave_api/config.py
+++ signwave_api/config.py
@@ -1,3 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql://root:@localhost/user_accounts"
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = (
+        "mysql://SignWave:2t}X6{Zoyu\C4?Q)60l/06Daco>h+^#@SignWave.mysql.pythonanywhere-services.com/SignWave$user_management"
+    )
     ASSEMBLYAI_API_KEY = "1d3be8d88bba446aac7c750f5a191612"
