af0a9e169a6df9be9e0b20255d0b361cfcdb56cc
Deshan0802 <d.deshan0802@gmail.com>
2024-03-15 01:41:20 +0530

    Add condion to check  bad requests for webhook

--- signwave_api/app/routes/git_update.py
+++ signwave_api/app/routes/git_update.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 import git
 
 git_update_bp = Blueprint("git-update", __name__)
@@ -6,10 +6,13 @@ git_update_bp = Blueprint("git-update", __name__)
 
 @git_update_bp.route("/git-update", methods=["POST"])
 def git_update():
-    repo = git.Repo("./SDGP")
-    origin = repo.remotes.origin
-    repo.create_head("main", origin.refs.main).set_tracking_branch(
-        origin.refs.main
-    ).checkout()
-    origin.pull()
-    return "", 200
+    if request.method == "POST":
+        repo = git.Repo("./SDGP")
+        origin = repo.remotes.origin
+        repo.create_head("main", origin.refs.main).set_tracking_branch(
+            origin.refs.main
+        ).checkout()
+        origin.pull()
+        return "", 200
+    else:
+        return "", 400
