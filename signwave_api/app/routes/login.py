688c6df3cd9dfb4c5d68fafe1a535bf047a65d33
Minduli lasandi <mindulilasandi@gmail.com>
2024-03-15 14:46:15 +0530

    Change response messages in backend for login

--- signwave_api/app/routes/login.py
+++ signwave_api/app/routes/login.py
@@ -7,21 +7,21 @@ login_bp = Blueprint("login", __name__)
 @login_bp.route("/login", methods=["POST"])
 def login():
     if not request.is_json:
-        return jsonify({"error": "Request must be JSON"}), 400
+        return jsonify({"message": "Request must be JSON"}), 400
 
     data = request.json
     username = data.get("username")
     password = data.get("password")
 
     if not username:
-        return jsonify({"error": "Username is required"}), 400
+        return jsonify({"message": "Username is required"}), 400
 
     if not password:
-        return jsonify({"error": "Password is required"}), 400
+        return jsonify({"message": "Password is required"}), 400
 
     user = User.query.filter_by(username=username, password=password).first()
 
     if user:
-        return jsonify({"success": True}), 200
+        return jsonify({"message": "Login Successful, Welcome back!"}), 200
     else:
-        return jsonify({"error": "Invalid username or password"}), 401
+        return jsonify({"message": "Invalid username or password"}), 401
