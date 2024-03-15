f1bb8f703b7ca4a6fa78ef9202a11d4e73e4a903
Deshan0802 <d.deshan0802@gmail.com>
2024-03-15 13:01:33 +0530

    Fix relative path for respoistory in git_update.py

--- signwave_api/app/routes/git_update.py
+++ signwave_api/app/routes/git_update.py
@@ -7,7 +7,7 @@ git_update_bp = Blueprint("git-update", __name__)
 @git_update_bp.route("/git-update", methods=["POST"])
 def git_update():
     if request.method == "POST":
-        repo = git.Repo("./SDGP")
+        repo = git.Repo("../SDGP")
         origin = repo.remotes.origin
         repo.create_head("main", origin.refs.main).set_tracking_branch(
             origin.refs.main
