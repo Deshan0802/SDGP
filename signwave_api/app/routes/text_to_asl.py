f881adc428ad13f03431a2df16e3d82ea6c82683
Romayle <romayle.dharmasena@gmail.com>
2024-03-19 21:41:36 +0530

    refactored the logic related to converting text to ASL video

--- signwave_api/app/routes/text_to_asl.py
+++ signwave_api/app/routes/text_to_asl.py
@@ -9,6 +9,7 @@ from pose_format.pose_visualizer import PoseVisualizer
 import requests
 import cv2
 
+previous = ''
 text_to_asl_bp = Blueprint("text-to-asl", __name__)
 
 
@@ -19,19 +20,48 @@ text_to_asl_bp = Blueprint("text-to-asl", __name__)
 #     print('Received text:', text)
 #     return jsonify({'message': 'Text received', 'text': text})
 
+
+
+def textToASLVideo(inputText):
+    '''
+    Returns the video file name
+            Parameters:
+                    input (int): the string of text to convert to ASL to text
+
+            Returns:
+                    video (str): file name of video which is created 
+    '''
+    
+    print('------------ Text to translate to ASL : ' + inputText)
+    apiForTextForASL = f"https://us-central1-sign-mt.cloudfunctions.net/spoken_text_to_signed_pose?text={inputText}&spoken=en&signed=ase"
+    response = ''
+    global previous
+    if(previous != inputText):
+        print("-----------------First call --------------")
+        response = requests.get(apiForTextForASL) 
+        if (response.status_code == 200):
+            previous = inputText
+            # read pose file  
+            pose = Pose.read(response.content)
+            v = PoseVisualizer(pose)
+            v.save_video(vid_name, v.draw()) 
+        else:
+            print(f"Error in Get Requests Error Code :{response.status_code}")
+    else:
+        print("-----------serve created vid on repeated requests---------")
+        
+    return vid_name
+         
+
 @text_to_asl_bp.route("/textToASL", methods=['GET'])
 def textToASL():
+    '''
+        Endpoint to call to translate text to ASL and get video
+    '''
+    print("---------- START: text to ASL--------")
     inputText = request.args.get('inputText')
-    apiForTextForASL = f"https://us-central1-sign-mt.cloudfunctions.net/spoken_text_to_signed_pose?text={inputText}&spoken=en&signed=ase"
-    print("inputText : " + inputText)
+    print('------------ Text parameter in Request : ' + inputText)
+    video = textToASLVideo(inputText)
+    print("---------- END: text to ASL----------")
+    return send_file(video) 
     
-    response = requests.get(apiForTextForASL)
-    
-    if(response.status_code == 200):
-        # read pose file  
-        pose = Pose.read(response.content)
-        v = PoseVisualizer(pose)
-        v.save_video(vid_name, v.draw())
-        return send_file(vid_name)    
-    else:
-        print(f"Error in Get Requests Error Code :{response.status_code}") 
