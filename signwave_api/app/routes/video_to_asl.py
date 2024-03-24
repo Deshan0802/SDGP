666767fd1ba9efa5699c63cbc3816746cfae6f01
Deshan0802 <d.deshan0802@gmail.com>
2024-03-24 11:05:16 +0530

    Change backend logic for video_to_asl page

--- signwave_api/app/routes/video_to_asl.py
+++ signwave_api/app/routes/video_to_asl.py
@@ -1,15 +1,29 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, send_file
 from app.helpers.assembly_ai import generate_transcript
 from app.helpers.mda_trnsl import translate
 
 video_to_asl_bp = Blueprint("video-to-asl", __name__)
 
+video_file = None
 
-@video_to_asl_bp.route("/video-to-asl", methods=["POST"])
-def video_to_asl():
-    if "video" not in request.files or request.files["video"].filename == "":
+
+@video_to_asl_bp.route("/upload-video", methods=["POST"])
+def get_file():
+    global video_file
+    if "video" not in request.files:
         return "", 400
     file = request.files["video"]
-    file.save(file.filename)
-    transcript = generate_transcript(file.filename)
-    video_files = translate(transcript)
+    if file.filename == "":
+        return "", 400
+    video_file = file.filename
+    file.save(video_file)
+    return "", 200
+
+
+@video_to_asl_bp.route("/download-translation", methods=["GET"])
+def send_translation():
+    global video_file
+    transcript = generate_transcript(video_file)
+    translation_video = translate(transcript)
+    translation_video_path = "../../" + translation_video
+    return send_file(translation_video_path)
